dcbaa1b98b4d8adec767d1cb9e9924fb6f2f142d
Dean Putney <mustardhamsters@gmail.com>
2013-04-02 15:37:59 -0700

    Indenting document.ready function

--- js/script.js
+++ js/script.js
@@ -5,38 +5,38 @@ function drawWindow(){
 
 jQuery(document).ready(function($) {
 
-// Draw map on page load
-drawWindow();
+  // Draw map on page load
+  drawWindow();
     
-// Redraw map on resize
-$(window).resize(function(){ drawWindow(); });
+  // Redraw map on resize
+  $(window).resize(function(){ drawWindow(); });
 
 
-// Panels
-// ==========
+  // Panels
+  // ==========
 
-// Hide all panels
-$('#panel-wrapper .panel').hide();
+  // Hide all panels
+  $('#panel-wrapper .panel').hide();
 
-// Show active/first panel
-var activePanel = $('#panel-tabs li.active:first');
-if (!activePanel.length) activePanel = $('#panel-tabs li:first');
+  // Show active/first panel
+  var activePanel = $('#panel-tabs li.active:first');
+  if (!activePanel.length) activePanel = $('#panel-tabs li:first');
 
-if (activePanel.length) {
-	activePanel.addClass('active');
-	var panelName = activePanel.attr('class').match(/panel-(\w+)/);
-	$('#' + panelName[1] + '-panel').show(); // Show active
-}
+  if (activePanel.length) {
+  	activePanel.addClass('active');
+  	var panelName = activePanel.attr('class').match(/panel-(\w+)/);
+  	$('#' + panelName[1] + '-panel').show(); // Show active
+  }
 
-// Show panel on tab click
-$('#panel-tabs li a').click(function() {
-	$('#panel-wrapper .panel').hide(); // Hide all
+  // Show panel on tab click
+  $('#panel-tabs li a').click(function() {
+  	$('#panel-wrapper .panel').hide(); // Hide all
 
-	var panelName = $(this).parent('li').attr('class').match(/panel-(\w+)/);
-	$('#' + panelName[1] + '-panel').show(); // Show clicked
+  	var panelName = $(this).parent('li').attr('class').match(/panel-(\w+)/);
+  	$('#' + panelName[1] + '-panel').show(); // Show clicked
 
-	$(this).blur(); // shh
-});
+  	$(this).blur(); // shh
+  });
 
 
 // end document.ready()
