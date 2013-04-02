4aeed34dd88dd21f63051d2f029c09d625ec55fd
SQ <de@detrimental.net>
2013-04-02 19:37:02 -0400

    How do you say Collapsing New Panels in German? Einstürzende Neutafel. #CommitJokes

--- main/main.js
+++ main/main.js
@@ -6,8 +6,11 @@ jQuery(document).ready(function($) {
 	$('#panel-wrapper .panel').hide();
 
 	// Show active/first panel
-	var activePanel = $('#panel-tabs li.active:first');
-	if (!activePanel.length) activePanel = $('#panel-tabs li:first');
+	var activePanel = $('#panel-tabs li.active:first'),
+		hashPanel   = $('#panel-tabs li.panel-' + document.location.hash.substr(1));
+
+	if (!activePanel.length) activePanel = $('#panel-tabs li.panel-pins');
+	if (hashPanel.length)    activePanel = hashPanel;
 
 	if (activePanel.length) {
 		activePanel.addClass('active');
@@ -15,14 +18,34 @@ jQuery(document).ready(function($) {
 		$('#' + panelName[1] + '-panel').show(); // Show active
 	}
 
+	// Hide panel if #panel-closed
+	if (document.location.hash == '#panel-closed')
+		$('#panel-wrapper').removeClass('open').addClass('closed');
+
 	// Show panel on tab click
 	$('#panel-tabs li a').click(function() {
-		$('#panel-wrapper .panel').hide(); // Hide all
-
-		var panelName = $(this).parent('li').attr('class').match(/panel-(\w+)/);
-		$('#' + panelName[1] + '-panel').show(); // Show clicked
+		var li      = $(this).parent('li'),
+			wrapper = $('#panel-wrapper'),
+			panelName;
 
 		$(this).blur(); // shh
+		
+		// Hide all
+		$('#panel-wrapper .panel').hide();
+
+		if (wrapper.hasClass('closed')) { // Open panel
+			wrapper.removeClass('closed').addClass('open');
+		} else if (li.hasClass('active')) { // Close panel
+			wrapper.removeClass('open').addClass('closed');
+			document.location.hash = 'panel-closed';
+			return;
+		}
+
+		$('#panel-tabs li.active').removeClass('active');
+
+		panelName = li.attr('class').match(/panel-(\w+)/);
+		$('#' + panelName[1] + '-panel').show(); // Show clicked
+		$('#panel-tabs li.panel-' + panelName[1]).addClass('active');
 	});
 
 
