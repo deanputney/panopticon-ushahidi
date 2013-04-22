1d36fca45afeac0e63759b5b6ca85a25e66bd37b
SQ <de@detrimental.net>
2013-04-22 02:37:50 -0400

    Hide #intro if there's a closed cookie or a hash

--- main/main.js
+++ main/main.js
@@ -145,6 +145,7 @@ jQuery(document).ready(function($) {
 	// Intro modal
 	$('#intro').click(function() {
 		$('#intro').fadeOut('fast');
+		$.cookie('hide_intro', '1', { expires: 7 });
 	})
 
 	$('#intro-video .placeholder').click(function() {
@@ -155,7 +156,10 @@ jQuery(document).ready(function($) {
 		});
 
 		return false;
-	})
+	});
+
+	// Hide it if we've got a Close cookie or hash to load
+	if ( $.cookie('hide_intro') != undefined || document.location.hash != '' ) $('#intro').hide();
 
 // end document.ready()
 });
@@ -180,3 +184,12 @@ m.prototype.observe=function(a,b,d){a=new k(this._target,a,b,d);c(this._target);
 !0};m.prototype.resume=function(){this._paused=!1};m.prototype._handler=function(a){if(!this._paused){var b={type:null,target:null,addedNodes:null,removedNodes:null,previousSibling:null,nextSibling:null,attributeName:null,attributeNamespace:null,oldValue:null};switch(a.type){case "DOMAttrModified":b.type="attributes";b.target=a.target;b.attributeName=a.attrName;b.oldValue=a.prevValue;break;case "DOMCharacerDataModified":b.type="characterData";b.target=c(a.target).parent().get(0);b.attributeName=a.attrName;
 b.oldValue=a.prevValue;break;case "DOMNodeInserted":b.type="childList";b.target=a.relatedNode;b.addedNodes=[a.target];b.removedNodes=[];break;case "DOMNodeRemoved":b.type="childList",b.target=a.relatedNode,b.addedNodes=[],b.removedNodes=[a.target]}for(a=0;a<this.patterns.length;a++){var d=this.patterns[a],e=d.match(b);e.length&&e.each(function(){d.handler.call(this,b)})}}};m.prototype._addEvent=function(a){this._events[a]||(c(this._target).on(a+this.NS,this._handler),this._events[a]=!0)};c.fn.observe=
 function(a,b,d){b?d||(d=b,b=null):(d=a,a=i);return this.each(function(){var e=c(this),f=e.data("observer");f||(f=n?new p(this):new m(this),e.data("observer",f));a=j(a);f.observe(a,b,d)})};c.fn.disconnect=function(a,b,d){a&&(b?d||(d=b,b=null):(d=a,a=i));return this.each(function(){var e=c(this),f=e.data("observer");f&&(a?(a=j(a),f.disconnect(a,b,d)):(f.disconnectAll(),e.removeData("observer")))})}})(jQuery);
+
+/*!
+* jQuery Cookie Plugin v1.3.1
+* https://github.com/carhartl/jquery-cookie
+*
+* Copyright 2013 Klaus Hartl
+* Released under the MIT license
+*/
+(function(factory){if(typeof define==="function"&&define.amd){define(["jquery"],factory)}else{factory(jQuery)}}(function($){var pluses=/\+/g;function raw(s){return s}function decoded(s){return decodeURIComponent(s.replace(pluses," "))}function converted(s){if(s.indexOf('"')===0){s=s.slice(1,-1).replace(/\\"/g,'"').replace(/\\\\/g,"\\")}try{return config.json?JSON.parse(s):s}catch(er){}}var config=$.cookie=function(key,value,options){if(value!==undefined){options=$.extend({},config.defaults,options);if(typeof options.expires==="number"){var days=options.expires,t=options.expires=new Date();t.setDate(t.getDate()+days)}value=config.json?JSON.stringify(value):String(value);return(document.cookie=[config.raw?key:encodeURIComponent(key),"=",config.raw?value:encodeURIComponent(value),options.expires?"; expires="+options.expires.toUTCString():"",options.path?"; path="+options.path:"",options.domain?"; domain="+options.domain:"",options.secure?"; secure":""].join(""))}var decode=config.raw?raw:decoded;var cookies=document.cookie.split("; ");var result=key?undefined:{};for(var i=0,l=cookies.length;i<l;i++){var parts=cookies[i].split("=");var name=decode(parts.shift());var cookie=decode(parts.join("="));if(key&&key===name){result=converted(cookie);break}if(!key){result[name]=converted(cookie)}}return result};config.defaults={};$.removeCookie=function(key,options){if($.cookie(key)!==undefined){$.cookie(key,"",$.extend({},options,{expires:-1}));return true}return false}}));
